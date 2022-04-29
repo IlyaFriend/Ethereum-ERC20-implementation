c63443f0454de7ce1537dd380e79ab300aa23b62
IlyaFriend <93038501+IlyaFriend@users.noreply.github.com>
2022-04-29 13:32:16 +0300

    Update biloverbenko.sol

--- biloverbenko.sol
+++ biloverbenko.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/24a0bc23cfe3fbc76f8f2510b78af1e948ae6651/contracts/utils/math/SafeMath.sol";
 
-contract TSH is ERC20 {
+contract BLV is ERC20 {
     constructor(uint256 initialSupply) ERC20("Biloverbenko", "BLV") {
         _mint(msg.sender, initialSupply);
     }
